0f0a9b4098ba39963ceecf3a9e8a68c1df5e6270
ynux <ynux@gmx.net>
2016-10-23 17:14:40 +0200

    introducing shift_series

--- api/app/Http/Controllers/EventsController.php
+++ api/app/Http/Controllers/EventsController.php
@@ -17,6 +17,16 @@ class EventsController extends Controller
 
     public function getEvent($id)
     { 
+        $event = DB::table('event')->where('url', $id)->first();
+        $comments = DB::table('event_comments')->where('event_id', $event->event_id)->get();
+        $shifts = DB::table('shift')->where('event_id', $event->event_id)->get();
+        var_dump($shifts);
+        die;
+        return response($event);
+        //return response()->json($event);
+    }
+    public function getEvent_fakesql($id)
+    { 
       // return response()->json(json_decode(file_get_contents(__DIR__.'/../../../event_1.json')));
         $event = DB::select('select * from event where url = \'testurl\'');
 
