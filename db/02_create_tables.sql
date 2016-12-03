4c10f7346d5b5a66520e665bd246c3968417cdb8
ynux <ynux@gmx.net>
2016-12-03 18:14:12 +0100

    with types, not series

--- db/02_create_tables.sql
+++ db/02_create_tables.sql
@@ -1,45 +1,38 @@
 use shiftplanaas;
 
-create table event (event_id int(30) unsigned PRIMARY KEY  AUTO_INCREMENT, 
+create table organization (organization_id int(30) unsigned PRIMARY KEY  AUTO_INCREMENT, 
 name varchar(120) NOT NULL,
 url_token_participants varchar(512) UNIQUE,
 url_token_admin varchar(512) UNIQUE,
-description varchar(300), 
-place varchar(120)
-);
+description varchar(300),
+creation_date datetime,
+update_date datetime);
 
-create table event_comments (event_comment_id int(30) unsigned PRIMARY KEY  AUTO_INCREMENT,
-event_id int(30) unsigned NOT NULL, 
+create table shift_type (shift_type int(30) unsigned PRIMARY KEY AUTO_INCREMENT,
+shift_id int(30) unsigned NOT NULL, 
 name varchar(300),
-comment varchar(120),
-date datetime);
+short_name varchar(10),
+description varchar(300), 
+location varchar(200),
+participants_min int(10) unsigned,
+participants_max int(10) unsigned,
+creation_date datetime,
+update_date datetime);
+
+create table shift (
+shift_id int(30) unsigned PRIMARY KEY  AUTO_INCREMENT,
+shift_type_id int(30) unsigned NOT NULL,
+description varchar(200),
+start_time datetime,
+end_time datetime,
+creation_date datetime,
+update_date datetime);
 
 create table shift_comments (shift_comment_id int(30) unsigned PRIMARY KEY AUTO_INCREMENT,
 shift_id int(30) unsigned NOT NULL, 
 name varchar(300),
 comment varchar(120),
-date datetime);
-
-create table shift_series (
-shift_series_id int(30) unsigned PRIMARY KEY  AUTO_INCREMENT,
-event_id int(30) unsigned NOT NULL,
-tasks varchar(300),
-participants_min int(10) unsigned,
-participants_max int(10) unsigned,
-description varchar(200),
-place varchar(120)
-) ;
-
-create table shift (
-shift_id int(30) unsigned PRIMARY KEY  AUTO_INCREMENT,
-shift_series_id int(30) unsigned NOT NULL,
-description varchar(200),
-place varchar(120),
-start_time datetime,
-end_time datetime,
-created datetime,
-last_change datetime
-);
+creation_date datetime);
 
 create table participants (
 participant_id int(30) unsigned PRIMARY KEY  AUTO_INCREMENT,
